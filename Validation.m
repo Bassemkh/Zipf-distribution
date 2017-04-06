23f8edf4629c55900adcf147f1cc018f0d75c29c
Joshephgit <khalfib@oregonstate.edu>
2017-04-06 02:24:28 -0700

    Add files via upload

---
+++ Validation.m
@@ -0,0 +1,21 @@
+% This script tests and validate the zipf distribution function.
+clear; clc; close all;
+m=10;
+alpha=1.5;
+n=1e5;
+X=zipfrnd( alpha,m );
+% tst
+occurance=zeros(1,m);
+sample=zipfrnd( alpha,m,n );
+for i=1:length(sample) 
+    occurance(sample(i))=occurance(sample(i))+1;
+end
+occurance=occurance/length(sample);
+
+Prob=zeros(1,m);
+for idx=1:m
+    Prob(idx)=(1/idx)^alpha;
+end
+Prob=Prob/sum(Prob);
+bar([Prob' ,occurance'])
+legend('real','esti')
